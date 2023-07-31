b873e2382de3a9d4b9a810a81c63231e32400327
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-07-31 09:49:08 -0400

    restructing

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 const express = require("express"),
-  http = require("http"),
-  fs = require("fs"),
-  url = require("url"),
   path = require("path"),
   morgan = require("morgan");
 
 const app = express();
 app.use(morgan("common"));
 
-http
-  .createServer((request, response) => {
-    let addr = request.url,
-      q = url.parse(addr, true),
-      filePath = "";
+app.get("/public", (req, res) => {
+  res.send("Welcome to Select Films.");
+});
 
-    fs.appendFile(
-      "log.txt",
-      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
-      (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Added to log.");
-        }
-      }
-    );
-    if (q.pathname.includes("documentation")) {
-      filePath = __dirname + "/documentation.html";
-    } else {
-      filePath = "index.js";
-    }
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        throw err;
-      }
+app.listen(8080, () => {
+  console.log("Your port is listening on 8080.");
+});
 
-      response.writeHead(200, { "Content-type": "text/html" });
-      response.write(data);
-      response.end();
-    });
-  })
-  .listen(8080),
-  console.log("My server is running on port 8080");
+// http
+//   .createServer((request, response) => {
+//     let addr = request.url,
+//       q = url.parse(addr, true),
+//       filePath = "";
+
+//     fs.appendFile(
+//       "log.txt",
+//       "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+//       (err) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log("Added to log.");
+//         }
+//       }
+//     );
+//     if (q.pathname.includes("documentation")) {
+//       filePath = __dirname + "/documentation.html";
+//     } else {
+//       filePath = "index.js";
+//     }
+//     fs.readFile(filePath, (err, data) => {
+//       if (err) {
+//         throw err;
+//       }
+
+//       response.writeHead(200, { "Content-type": "text/html" });
+//       response.write(data);
+//       response.end();
+//     });
+//   })
+//   .listen(8080),
+//   console.log("My server is running on port 8080");

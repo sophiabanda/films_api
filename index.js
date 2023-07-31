d9a2545789a7b4dee14908b7c5c191453737507a
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-07-31 09:12:23 -0400

    file restructure

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const express = require("express"),
-  app = express(),
+  http = require("http"),
+  fs = require("fs"),
+  url = require("url"),
+  path = require("path"),
   morgan = require("morgan");
 
+const app = express();
 app.use(morgan("common"));
 
-let topBooks = [
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    author: "J.K. Rowling",
-  },
-  {
-    title: "Lord of the Rings",
-    author: "J.R.R. Tolkien",
-  },
-  {
-    title: "Twilight",
-    author: "Stephanie Meyer",
-  },
-];
+http
+  .createServer((request, response) => {
+    let addr = request.url,
+      q = url.parse(addr, true),
+      filePath = "";
 
-//express.METHOD(PATH, HANDLER)
-app.get("/", (req, res) => {
-  res.send("Welcome to my book club!");
-});
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+    } else {
+      filePath = "index.js";
+    }
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
 
-app.get("/documentation", (req, res) => {
-  res.sendFile("public/documentation.html", { root: __dirname });
-});
-
-app.get("/books", (req, res) => {
-  res.json(topBooks);
-});
+      response.writeHead(200, { "Content-type": "text/html" });
+      response.write(data);
+      response.end();
+    });
+  })
+  .listen(8080),
+  console.log("My server is running on port 8080");
